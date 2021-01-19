7d1f71493d09bc7df88f82e699634f8b26f7ed28
ArclightMike <arclightmike@outlook.com>
2021-01-19 13:11:02 -0600

    Booster Pack routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SetComponent } from './set/set.component';
+import { BoosterPackComponent } from './booster-pack/booster-pack.component';
 
 const routes: Routes = [
   {path: 'set', component: SetComponent},
+  {path: 'boosterpack', component: BoosterPackComponent},
   {path: '', redirectTo: '/set', pathMatch: 'full'},
 ]
 
