e997d7ce06e05bd5d7f00f85172bcb108ca15036
ArclightMike <arclightmike@outlook.com>
2021-01-19 14:18:27 -0600

    Set up booster pack component.

--- src/app/booster-pack/booster-pack.component.ts
+++ src/app/booster-pack/booster-pack.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../card';
+import { CardService } from '../card.service';
 
 @Component({
   selector: 'app-booster-pack',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoosterPackComponent implements OnInit {
 
-  constructor() { }
+  commons: Card[];
+  uncommons: Card[];
+  rares: Card[];
+  mythics: Card[];
+
+  constructor(private cardService: CardService) { }
 
   ngOnInit() {
+    this.getMythics();
+    this.getRares();
+    this.getUncommons();
+    this.getCommons();
+  }
+
+  getCommons(): void {
+  this.cardService.getCommons().subscribe(cards => this.commons = cards);
+  }
+
+  getUncommons(): void {
+  this.cardService.getUncommons().subscribe(cards => this.uncommons = cards);
+  }
+
+  getRares(): void {
+  this.cardService.getRares().subscribe(cards => this.rares = cards);
+  }
+
+  getMythics(): void {
+  this.cardService.getMythics().subscribe(cards => this.mythics = cards);
   }
 
 }
